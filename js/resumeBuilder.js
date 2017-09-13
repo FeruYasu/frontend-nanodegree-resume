0037284dade29eaec4eb0d2a66269ed55e0f0519
FeruYasu <30644819+FeruYasu@users.noreply.github.com>
2017-09-12 23:50:09 -0300

    Aula alguma coisa

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,68 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+    var bio = {
+      "name" : "Fernando Yasumoto",
+      "role" : "Web Developer",
+      "contacts": {
+        "mobile" : "99598-2635",
+        "email" : "fernando@momo.com.br",
+        "github": "feruyatsu",
+        "twitter": "@feruyatsu",
+        "location": "Parana"
+      },
+      "welcomeMessage": "lalalalalalalalal alalala la la la",
+      "skills": ["bonito", "inteligente", "elegante","forte"],
+      "bioPic": "images/fry.jpg"
+    }
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Condor Hotel",
+			"title": "Financeiro",
+			"dates": "November 2016- Future",
+			"description": "Pagar as contas",
+			"location": "Curitiba"
+		},
+		{
+			"employer": "Denso",
+			"title": "Estagiario",
+			"dates": "November 2014- 2016",
+			"description": "Desenhar",
+			"location": "Curitiba"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Condor Hotel",
+			"date": "Financeiro",
+			"description": "November 2016- Future",
+			"images": "Pagar as contas"
+		},
+		{
+			"title": "Condor Hotel",
+			"date": "Financeiro",
+			"description": "November 2016- Future",
+			"images": "Pagar as contas"
+		}
+	]
+}
+
+
+var education = {
+	"schools": {
+		"name": "Escola pastorzinho",
+		"location": "Curitiba- Pr",
+		"degree": "jardim de infancia",
+		"majors": [],
+		"dates": "oi",
+		"url": "www.site.com.br",
+		"onlineCourses": {
+			"title": "Udacity Web Dev",
+			"school": "Udacity",
+			"dates": "2016",
+			"url": "www.curso.com.br"
+		}
+	}
+}
